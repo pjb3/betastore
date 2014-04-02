e8519cdf91b603c9acfa6171e487372ccc67f199
Paul Barry <mail@paulbarry.com>
2014-04-01 22:35:33 -0400

    Added integration test for managing products

---
+++ test/integration/admin_products_test.rb
@@ -0,0 +1,26 @@
+
+require 'test_helper'
+
+class AdminProductsTest < ActionDispatch::IntegrationTest
+  def test_manage
+    visit "/admin/products"
+
+    click_on "New Product"
+    fill_in "Name", with: "Awesome"
+    fill_in "Price", with: "9.99"
+    click_on "Save"
+
+    click_on "Awesome"
+    assert page.has_content?('9.99')
+    click_on "Edit"
+
+    fill_in "Price", with: "18.99"
+    click_on "Save"
+
+    click_on "Awesome"
+    assert page.has_content?('18.99')
+    click_on "Delete"
+
+    refute page.has_content?('Awesome')
+  end
+end
