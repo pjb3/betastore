0fe7a172ab910b87d3f4bb5345774846b378a552
Paul Barry <mail@paulbarry.com>
2014-04-14 21:04:28 -0400

    Remove failing code

--- test/integration/create_subscription_test.rb
+++ test/integration/create_subscription_test.rb
@@ -4,9 +4,7 @@ class CreateSubscriptionTest < ActionDispatch::IntegrationTest
 
   def test_create_subscription
     visit "/"
-    click_on "Fail"
     fill_in "Find out when we go live", with: 'test@example.com'
-    # asdkfsldfjsdklfjsdfkjds
     click_on "Sign Up"
     assert page.has_content?('Subscription was successfully created.')
     assert_equal 'test@example.com', Subscription.last.email
