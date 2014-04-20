651a638c01e901c9765ad8f9ef7aeef50813b3f4
Paul Barry <mail@paulbarry.com>
2014-04-20 10:43:10 -0400

    Send verification email when Customer is created, also log error when verifying a customer token

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,12 +7,19 @@ class Customer < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
+  after_create :send_verification_email
+
   def self.verify(token)
     customer_id = Rails.application.message_verifier('customer').verify(token)
     customer = Customer.find(customer_id)
     customer.update!(verified_at: Time.current) unless customer.verified_at
     customer
-  rescue
+  rescue => ex
+    logger.error "Could not verify user: #{ex.class} #{ex.message}"
     nil
+  end
+
+  def send_verification_email
+    CustomerMailer.welcome(self).deliver
   end
 end
