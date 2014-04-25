b658b23e88303a744b1ae42c5888b8388b5058cc
Paul Barry <mail@paulbarry.com>
2014-04-24 20:42:18 -0400

    Switched to pg_search for better switch

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,14 @@
 class Product < ActiveRecord::Base
+  include PgSearch
+
   has_many :categorizations
   has_many :categories, :through => :categorizations
 
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0, allow_blank: true }
 
-  scope :search, -> (q) { where('name like ?', "%#{q}%") }
+  pg_search_scope :search, :against => :name
   scope :min_price, -> (price) { where('price >= ?', price) }
   scope :max_price, -> (price) { where('price <= ?', price) }
+
 end
