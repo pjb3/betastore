0b358abb9eb8bb8f83c51c989795315f19cd3bb2
Paul Barry <mail@paulbarry.com>
2014-04-24 20:39:50 -0400

    Rename param, added back link and made filters generic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,12 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order('name')
 
-    if params[:q].present?
-      @products = @products.search(params[:q])
+    for param in %i[search min_price max_price]
+      if params[param].present?
+        @products = @products.send(param, params[param])
+      end
     end
+
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ActiveRecord::Base
   validates :price, presence: true, numericality: { greater_than: 0, allow_blank: true }
 
   scope :search, -> (q) { where('name like ?', "%#{q}%") }
+  scope :min_price, -> (price) { where('price >= ?', price) }
+  scope :max_price, -> (price) { where('price <= ?', price) }
 end
